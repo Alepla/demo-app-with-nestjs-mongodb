6f04cdf55ae771bab09ce9a82484acd77ec20a6d
Alejandro <aleplacambra@gmail.com>
2021-03-09 11:10:23 +0100

    adding the crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AboutModule } from './about/about.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AboutModule],
+  imports: [
+    AboutModule,
+    MongooseModule.forRoot('mongodb://localhost:27019/mongodb_nest'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
